e22b205ce5d45c0ce385a3555380264a377a63b8
david-i-berry <dberry@wmo.int>
2023-04-02 12:26:08 +0200

    Addition of vuejs files.

---
+++ {{cookiecutter.process_id}}/vue/view/{{cookiecutter.component}}.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <{{cookiecutter.component}} title="{{cookiecutter.component}}" content="This page is under construction" />
+  </div>
+</template>
+
+<script>
+import {{cookiecutter.component | title | replace('-','')}} from './../web-components/{{cookiecutter.component}}.vue';
+
+
+export default {
+  name: 'MyView',
+  components: {
+    {{cookiecutter.component | title | replace('-','')}},
+  },
+};
+</script>
+
+<style>
+</style>
